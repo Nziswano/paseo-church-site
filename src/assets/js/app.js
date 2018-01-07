f4eda6ea54d09d0e8daa99477389d8ff9fa89346
Johan Martin <martin.johan@johan-martin.com>
2018-01-07 04:11:38 +0200

    added video and first basic site

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,12 +1,31 @@
 import $ from 'jquery';
 import whatInput from 'what-input';
-
+import * as Webfont from "webfontloader";
 window.$ = $;
+
+Webfont.load({
+  google: {
+    families: [
+      "Merriweather Sans",
+      "Nunito",
+      "Roboto",
+      "Josefin Sans"
+  ],
+  },
+});
+
 
 // import Foundation from 'foundation-sites';
 // If you want to pick and choose which modules to include, comment out the above and uncomment
 // the line below
 import './lib/foundation-explicit-pieces';
+
+
+
+/* 
+<link href="https://fonts.googleapis.com/css?family=Merriweather+Sans|Nunito|Roboto" rel="stylesheet">
+*/
+
 
 
 $(document).foundation();
