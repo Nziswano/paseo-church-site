40f1d8a89b189d94f233c20ac9e6e4f59329a738
Johan Martin <martin.johan@johan-martin.com>
2018-01-11 04:00:16 +0200

    keep latest

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,4 +1,6 @@
 import $ from 'jquery';
+import Vue from 'vue';
+import PaseoContactUs from "paseo_contact_us";
 import whatInput from 'what-input';
 import * as Webfont from "webfontloader";
 window.$ = $;
@@ -14,6 +16,20 @@ Webfont.load({
   ],
   },
 });
+
+
+let siteUrl = "http://localhost:5000/register";
+let captchaKey = "6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI";
+
+const app = new Vue({
+  el: "#contact-form",
+  render: (h) => h(PaseoContactUs, {
+    props: {
+      capcha: captchaKey,
+      url: siteUrl,
+    }
+  })
+})
 
 import './lib/foundation-explicit-pieces';
 
